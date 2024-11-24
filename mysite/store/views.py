9f4ea900e4700b52a2711405ed3d389f83483fb7
Connor Jones <connorjones1@mail.weber.edu>
2024-11-24 16:06:49 -0700

    save, added home page view

--- mysite/store/views.py
+++ mysite/store/views.py
@@ -7,7 +7,7 @@ from django.core.paginator import Paginator
 # Create your views here.
 def home(request):
     products = Product.objects.all()
-    return render(request, 'store/home.html', {'products': products})
+    return render(request, 'store/home.html') #, {'products': products}
 
 
 
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile')
+        else:
+            messages.error(request, 'Invalid username or password')
+    return render(request, 'users/login.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists')
+        else:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            login(request, user)
+            return redirect('profile')
+    return render(request, 'users/register.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def profile_view(request):
+    return render(request, 'users/profile.html')
