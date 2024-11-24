6312393e7582adfcd349aaac9eb5ac712d38ccd1
Connor Jones <connorjones1@mail.weber.edu>
2024-11-24 14:33:31 -0700

    created product model

--- mysite/store/models.py
+++ mysite/store/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+
+    def __str__(self):
+            return self.name
+
+    Category_choices = [
+        ('shorts', 'Shorts'),
+        ('shirts', 'Shirts')
+    ]
+
+    Gender_choices = [
+        ('men', "Men's"),
+        ('women', "Women's")
+    ]
+
+    name = models.CharField(max_length=200)
+    #image = models.ImageField(upload_to='product_images/')
+    sizes = models.JSONField()  # Example: ["S", "M", "L"]
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    gender = models.CharField(max_length=10, choices=Gender_choices)
+    category = models.CharField(max_length=10, choices=Category_choices)
+    stock = models.PositiveIntegerField()
